7abb97ddfda29502ff534de2796e1c527e9adc43
andreadldev <andrea.delisi2@gmail.com>
2023-01-19 18:48:41 +0100

    Creazione model e seeder

--- database/migrations/2023_01_19_171623_create_comics_table.php
+++ database/migrations/2023_01_19_171623_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb', 200);
+            $table->string('price', 50);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 50);
             $table->timestamps();
         });
     }
